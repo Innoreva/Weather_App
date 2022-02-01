a241b348af240d47873cf26f850f6dc55f44d9ec
Subhadiptech <2020ugee055@nitjsr.ac.in>
2022-02-01 21:34:04 +0530

    basic fixes

--- app/src/main/java/com/ersubhadip/instantweather/ui/MainActivity.kt
+++ app/src/main/java/com/ersubhadip/instantweather/ui/MainActivity.kt
@@ -1,10 +1,12 @@
-package com.ersubhadip.instantweather
+package com.ersubhadip.instantweather.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
+import com.ersubhadip.instantweather.R
+import com.ersubhadip.instantweather.api.RetrofitInstance
 import retrofit2.HttpException
 import java.io.IOException
 
