b493fd13b648f89bb9bedc79771b9e3c589f2983
Subhadiptech <2020ugee055@nitjsr.ac.in>
2022-02-03 02:22:27 +0530

    added binding to the layout

--- app/src/main/java/com/ersubhadip/instantweather/ui/MainActivity.kt
+++ app/src/main/java/com/ersubhadip/instantweather/ui/MainActivity.kt
@@ -7,15 +7,16 @@ import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import com.ersubhadip.instantweather.R
 import com.ersubhadip.instantweather.api.RetrofitInstance
+import com.ersubhadip.instantweather.databinding.ActivityMainBinding
 import retrofit2.HttpException
 import java.io.IOException
 
-const val TAG="#### MainActivity"
 class MainActivity : AppCompatActivity() {
-
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 
     }
--- app/src/main/java/com/ersubhadip/instantweather/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/ersubhadip/instantweather/viewmodel/MainViewModel.kt
@@ -1,12 +1,8 @@
 package com.ersubhadip.instantweather.viewmodel
-
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.ersubhadip.instantweather.ui.TAG
 import kotlinx.coroutines.launch
-import retrofit2.HttpException
-import java.io.IOException
+
 
 class MainViewModel(val repository: ApiRepository) : ViewModel() {
 
