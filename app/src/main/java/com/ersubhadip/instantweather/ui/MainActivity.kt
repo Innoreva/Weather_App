740a85d08aa2d2ef25ac53ec74b9d24c2abdd2a1
Subhadiptech <2020ugee055@nitjsr.ac.in>
2022-02-04 03:48:08 +0530

    nav Bar Library Implemented

--- app/src/main/java/com/ersubhadip/instantweather/ui/MainActivity.kt
+++ app/src/main/java/com/ersubhadip/instantweather/ui/MainActivity.kt
@@ -2,40 +2,47 @@ package com.ersubhadip.instantweather.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
+import android.widget.Toast.LENGTH_SHORT
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.ersubhadip.instantweather.R
-import com.ersubhadip.instantweather.api.RetrofitInstance
-import com.ersubhadip.instantweather.databinding.ActivityMainBinding
-import retrofit2.HttpException
-import java.io.IOException
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation
+
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
+
+        //setting up navBar
+        var bottomNavigation:MeowBottomNavigation=findViewById(R.id.bottomNavigationView)
+        bottomNavigation.add(MeowBottomNavigation.Model(1,R.drawable.ic_weather))
+        bottomNavigation.add(MeowBottomNavigation.Model(2,R.drawable.ic_forecast))
+        bottomNavigation.add(MeowBottomNavigation.Model(3,R.drawable.ic_graph))
+
+        bottomNavigation.show(1,true)
+
+        bottomNavigation.setOnClickMenuListener {
+            when(it.id){
+                1->Toast.makeText(this@MainActivity,"Home",LENGTH_SHORT).show()
+
+                2->Toast.makeText(this@MainActivity,"Forecast",LENGTH_SHORT).show()
+
+                3->Toast.makeText(this@MainActivity,"Weather Analytics",LENGTH_SHORT).show()
+
+                else -> Toast.makeText(this@MainActivity,"Hello",LENGTH_SHORT).show()
+            }
+        }
 
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
-        val navController = navHostFragment.navController
+//        val navHostFragment =
+//            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+//        val navController = navHostFragment.navController
 
-        // setting up Action bar with Navigation Component
-        val appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment, R.id.graphFragment, R.id.navHostFragment))
-        setupActionBarWithNavController(navController,appBarConfiguration)
-
-        binding.bottomNavigationView.setupWithNavController(navController)
 
     }
 }
