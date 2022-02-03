eb2e1fd385adc23f14fed411b20a5b976f5100ef
pareekdevansh <devanshpareek863@gmail.com>
2022-02-04 01:07:21 +0530

    Added basic Navigation for the app

--- app/src/main/java/com/ersubhadip/instantweather/ui/MainActivity.kt
+++ app/src/main/java/com/ersubhadip/instantweather/ui/MainActivity.kt
@@ -4,7 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.ersubhadip.instantweather.R
 import com.ersubhadip.instantweather.api.RetrofitInstance
 import com.ersubhadip.instantweather.databinding.ActivityMainBinding
@@ -12,12 +20,22 @@ import retrofit2.HttpException
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding:ActivityMainBinding
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        // setting up Action bar with Navigation Component
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment, R.id.graphFragment, R.id.navHostFragment))
+        setupActionBarWithNavController(navController,appBarConfiguration)
+
+        binding.bottomNavigationView.setupWithNavController(navController)
 
     }
 }
