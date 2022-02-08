38abd11436ce2b45a6715281199b2b5c084a2c37
pareekdevansh <devanshpareek863@gmail.com>
2022-02-09 01:30:34 +0530

    fixed UI related things along with Navigation

--- app/src/main/java/com/ersubhadip/instantweather/ui/MainActivity.kt
+++ app/src/main/java/com/ersubhadip/instantweather/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ersubhadip.instantweather.ui
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -9,6 +10,7 @@ import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.transition.FragmentTransitionSupport
@@ -22,53 +24,77 @@ import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-    //todo:setup accent color
-    //todo:font
-    //todo:fragment transaction deprecate -> NavGraph
-    //todo:colors.xml
-    //todo:current weather fragment -> ?
-    //todo:forced white theme
 
-    lateinit var vm:MainViewModel
+    //DONE:
+    //setup accent color
+    //colors.xml
+    //setting up the navigation for bottom bar
+
+    //TODO
+    //font -> pending
+    //current weather fragment -> ?
+    //forced white theme
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //setting up navBar
-        val bottomNavigation:MeowBottomNavigation=findViewById(R.id.bottomNavigationView)
-        bottomNavigation.add(MeowBottomNavigation.Model(1,R.drawable.ic_forecast))
-        bottomNavigation.add(MeowBottomNavigation.Model(2,R.drawable.ic_weather))
-        bottomNavigation.add(MeowBottomNavigation.Model(3,R.drawable.ic_graph))
-
-        bottomNavigation.show(2,true)
-
-        bottomNavigation.setOnClickMenuListener {
-            when(it.id){
-                1->Toast.makeText(this@MainActivity,"Forecast",LENGTH_SHORT).show()
-
-                2->Toast.makeText(this@MainActivity,"Home",LENGTH_SHORT).show()
-
-                3->Toast.makeText(this@MainActivity,"Weather Analytics",LENGTH_SHORT).show()
-
-                else -> Toast.makeText(this@MainActivity,"Hello",LENGTH_SHORT).show()
-            }
-        }
 
 
 //        val navHostFragment =
 //            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
 //        val navController = navHostFragment.navController
 
-        vm = ViewModelProvider(this)[MainViewModel::class.java];
-        GlobalScope.launch (Dispatchers.Main){
+        //setting up navBar
+        val bottomNavigation:MeowBottomNavigation = findViewById(R.id.bottomNavigationView)
+        bottomNavigation.add(MeowBottomNavigation.Model(R.string.forecastFragment ,R.drawable.ic_forecast))
+        bottomNavigation.add(MeowBottomNavigation.Model(R.string.homeFragment,R.drawable.ic_weather))
+        bottomNavigation.add(MeowBottomNavigation.Model(R.string.graphFragment,R.drawable.ic_graph))
 
-            vm.getCurrentWeatherVM()
+        bottomNavigation.show(R.string.homeFragment,true)
 
+
+        val homeFragment = HomeFragment()
+        val graphFragment = GraphFragment()
+        val forecastFragment = ForecastFragment()
+
+        setFragment(homeFragment, R.string.homeFragment)
+
+
+        bottomNavigation.setOnClickMenuListener {
+            when(it.id){
+                R.string.forecastFragment-> {
+                    Toast.makeText(this@MainActivity, "Forecast", LENGTH_SHORT).show()
+                    setFragment(forecastFragment, R.string.forecastFragment)
+
+                }
+
+                R.string.homeFragment-> {
+                    Toast.makeText(this@MainActivity, "Home", LENGTH_SHORT).show()
+                    setFragment(homeFragment , R.string.homeFragment)
+
+                }
+
+                R.string.graphFragment-> {
+                    Toast.makeText(this@MainActivity, "Weather Analytics", LENGTH_SHORT).show()
+                    setFragment(graphFragment ,R.string.graphFragment)
+                }
+
+                else -> Toast.makeText(this@MainActivity,"Hello",LENGTH_SHORT).show()
+            }
         }
 
+    }
 
+    private fun setFragment(fragment : Fragment, label : Int ) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(
+                R.id.flFragmentContainer, fragment
+            )
+            commit()
+            this@MainActivity.setTitle(label)
 
+        }
     }
 
 
