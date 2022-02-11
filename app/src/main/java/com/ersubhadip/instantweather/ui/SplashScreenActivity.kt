45cf6c1539df20a950c05ef8349ded37f91732c2
Subhadiptech <2020ugee055@nitjsr.ac.in>
2022-02-11 18:47:19 +0530

    layout fixes, Splash Screen layout and intent enhanced

--- app/src/main/java/com/ersubhadip/instantweather/ui/SplashScreenActivity.kt
+++ app/src/main/java/com/ersubhadip/instantweather/ui/SplashScreenActivity.kt
@@ -1,29 +1,46 @@
 package com.ersubhadip.instantweather.ui
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
-
 import com.ersubhadip.instantweather.R
+import com.nitish.typewriterview.TypeWriterView
+import com.nitish.typewriterview.TypeWriterView.OnAnimationChangeListener
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    //todo:ViewBinding not getting applied in SplashActivity - Do Later
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_splash_screen)
 
         supportActionBar?.hide()
-        //TODO : If possible then use SplashScreen with theme/Splash API/Fragment (to keep app lightweight😁)
-        lifecycleScope.launch(Dispatchers.Main) {
-            delay(1000L)
+
+        //delay for splash
+//        lifecycleScope.launch(Dispatchers.Main) {
+//            delay(1000L)
+//            val intent= Intent(this@SplashScreenActivity, MainActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//            //fix - nav component will be used later
+//        }
+
+        //TypeWriter Text
+         val typing = findViewById<TypeWriterView>(R.id.typeTv)
+        typing.animateText("Get Live Weather Report \nAnytime, Anywhere")
+        typing.setCharacterDelay(100L)
+
+        //some junk yet important - Intent after Animations completes
+        typing.setOnAnimationChangeListener(OnAnimationChangeListener {
             val intent= Intent(this@SplashScreenActivity, MainActivity::class.java)
             startActivity(intent)
             finish()
-            //fix - nav component will be used later
-        }
+        })
+
     }
 }
